3454395ccc527040941ef31883d737447072c733
kelven <kelvenstone@163.com>
2017-04-27 14:40:12 +0800

    edit deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:kelvenstone/lichenglish.git"
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 # Default deploy_to directory is /var/www/my_app_name
- set :deploy_to, '/home/apps/licheng'
+ set :deploy_to, '/home/apps/lichenglish'
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 # You can configure the Airbrussh format using :format_options.
