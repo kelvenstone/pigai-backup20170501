4d315fb832ddd9fdcfeb823f73f21d15f8a7767d
kelven <kelvenstone@163.com>
2017-04-27 13:16:30 +0800

    add capistrano file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,27 @@
 # config valid only for current version of Capistrano
 lock "3.8.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "lichenglish"
+set :repo_url, "git@github.com:kelvenstone/lichenglish.git"
+
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# Default deploy_to directory is /var/www/my_app_name
+ set :deploy_to, '/home/apps/licheng'
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capi strano.log', color: :auto, truncate: :auto
+# Default value for :pty is false
+# set :pty, true
+# Default value for :linked_files is []
+# append :linked_files, 'config/database.yml', 'config/secrets.y ml'
+set :linked_files, fetch(:linked_files, []).push('config/databas e.yml', 'config/secrets.yml')
+# Default value for linked_dirs is []
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sock ets', 'public/system'
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids ', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
