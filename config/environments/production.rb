752fccff67353d0379da00a634b0159388293c0d
kelven <kelvenstone@163.com>
2017-04-28 19:09:01 +0800

    edit forth produnction.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,10 +23,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # add follow two rows in April.28
-  # config.assets.compile = true
+  config.assets.compile = true
   # config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
